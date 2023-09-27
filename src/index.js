6c67825bdadd04d9c862e7435084565238da3dd0
Mariusz-72 <mmbwdap@gmail.com>
2023-09-27 20:25:49 +0200

    update 1.4

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@
 
 
 import axios from 'axios';
-
 axios.defaults.headers.common['x-api-key'] = 'live_iPaBzyy1iOjvPQNHzNKNExHWu1HC402dvsZ3klFHj2GCZBHTFhAkZ1tIADh4TvP';
+import SlimSelect from 'slim-select';
+import Notiflix from 'notiflix';
+
+const breedSelect = document.getElementById('breed-select');
+const loader = document.getElementById('loader');
+
+
+
+const slim = new SlimSelect({
+    select: '#breed-select',
+    placeholder: 'Select a breed...',
+})
